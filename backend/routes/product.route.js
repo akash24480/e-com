1655206e34be3a2de65a67cdd7e0a21149e107fc
Akash Kumar <akash19g245@gmail.com>
2025-02-12 22:59:38 +0530

    Product route is completed

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getAllProducts} from '../controller/product.controller.js'
+import {deleteProduct, getAllProducts, getProductsByCategory, getRecommendedProducts, toggleFeaturedProduct} from '../controller/product.controller.js'
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js'
 import { getFeaturedProducts, createProduct } from '../controller/product.controller.js'
 
@@ -8,8 +8,11 @@ const router = express.Router()
 
 router.get("/", protectRoute, adminRoute, getAllProducts)
 router.get("/featured", getFeaturedProducts)
+router.get("/category/:category", getProductsByCategory)
+router.get("/recommendations", getRecommendedProducts)
 router.post("/", protectRoute, adminRoute, createProduct);
-router.delete("/:id", protectRoute, adminRoute)
+router.patch("/:id", protectRoute, adminRoute, toggleFeaturedProduct)
+router.delete("/:id", protectRoute, adminRoute, deleteProduct)
 
 
 export default router
