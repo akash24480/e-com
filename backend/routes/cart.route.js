9bfc34fe17550642affe5d02e3d4fcef4aae02fc
Akash Kumar <akash19g245@gmail.com>
2025-02-13 22:33:39 +0530

    Coupon Route is completed

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -10,4 +10,5 @@ router.post("/",protectRoute, addToCart)
 router.delete("/", protectRoute, removeAllFromCart)
 router.put("/:id", protectRoute, updateQuantity)
 
- 
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import productRoutes from './routes/product.route.js';
-import cartRoutes from "./routes/cart.route.js"
+import cartRoutes from './routes/cart.route.js'
+import couponRoutes from './routes/coupon.route.js'
 import { connectDB } from './lib/bd.js'
 import cookieParser from 'cookie-parser'
 
@@ -18,9 +19,10 @@ app.use(cookieParser())
 
 
 
-app.use("/api/auth", authRoutes)
-app.use("/api/products", productRoutes)
-app.use("/api/cart", carRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes)
 
 
 
